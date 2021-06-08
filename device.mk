b4ff4fb552e4e6892318d5ecb0d94a32740237f0
GtrCraft <nickvanbokhorst@hotmail.com>
2021-06-08 14:04:12 +0200

    surya: We don't have oem cert

--- device.mk
+++ device.mk
@@ -30,10 +30,6 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
 
-# OEM otacert
-PRODUCT_EXTRA_RECOVERY_KEYS += \
-	 vendor/recovery/security/miui
-
 PRODUCT_COPY_FILES += \
     $(OUT_DIR)/target/product/surya/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
 
